8a07759b90e4c83ae14c055953b291766cf15eef
Gabriel Andrade <andrade.gabriel2295@gmail.com>
2024-11-04 22:43:16 -0300

    feat: implements finance table

--- src/pages/finance/index.tsx
+++ src/pages/finance/index.tsx
@@ -1,6 +1,37 @@
 import { useEffect, useState } from "react";
 import { Invoice } from "../../modules/finance/types";
 import { FinanceHandler } from "../../modules/finance/handler";
+import { AppTable } from "../../components/AppTable/AppTable";
+import { EditOutlined } from "@ant-design/icons";
+
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'invoiceId',
+    key: 'invoiceId',
+  },
+  {
+    title: 'Descrição',
+    dataIndex: 'title',
+    key: 'title',
+  },
+  {
+    title: 'Mês',
+    dataIndex: 'month',
+    key: 'month',
+  },
+  {
+    title: 'Ano',
+    dataIndex: 'year',
+    key: 'year',
+  },
+  {
+    title: 'Ação',
+    key: 'action',
+    render: () => {return <><a><EditOutlined /></a></>}
+  },
+];
 
 const FinancePage = () => {
   const [invoices, setInvoices] = useState<Invoice[]>([]);
@@ -24,10 +55,8 @@ const FinancePage = () => {
   
   return (
     <>
-      <h1>finance page</h1>
-      <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
+      <AppTable dataSource={invoices} columns={columns}/>
+
     </>
   );
 };
