07a7212ee4ebcb915c4611bf70bb8288bfacc240
Gabriel Andrade <andrade.gabriel2295@gmail.com>
2025-02-17 22:10:19 -0300

    feat: update layout to horizontal header

--- src/components/HmPage/index.tsx
+++ src/components/HmPage/index.tsx
@@ -1,54 +1,68 @@
 import './styles.scss'
-import { AppstoreOutlined, BarChartOutlined, CloudOutlined, ShopOutlined, TeamOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
-import { Layout, Menu, MenuProps } from "antd"
+import { BarChartOutlined, FileDoneOutlined, VideoCameraOutlined } from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, MenuProps, theme } from "antd"
 import { Content, Footer, Header } from "antd/es/layout/layout";
-import Sider from "antd/es/layout/Sider"
 import React from "react";
 
 const items: MenuProps['items'] = [
-    UserOutlined,
-    VideoCameraOutlined,
-    UploadOutlined,
-    BarChartOutlined,
-    CloudOutlined,
-    AppstoreOutlined,
-    TeamOutlined,
-    ShopOutlined,
-].map((icon, index) => ({
-    key: String(index + 1),
-    icon: React.createElement(icon),
-    label: `nav ${index + 1}`,
-}));
+  {
+    key: '1',
+    icon: <FileDoneOutlined />,
+    label: 'Finanças',
+  },
+  {
+    key: '2',
+    icon: <VideoCameraOutlined />,
+    label: 'Câmera',
+  },
+  {
+    key: '3',
+    icon: <BarChartOutlined />,
+    label: 'Relatórios',
+  },
+];
 
 type HmPageProps = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 export const HmPage: React.FC<HmPageProps> = ({ children }) => {
-    return (
-        <Layout hasSider className='page'>
-            <Sider className='sider'>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-            </Sider>
-            <Layout>
-                <Header style={{ padding: 0, background: 'yellow' }} />
-                <Content style={{ margin: '24px 16px 0', overflow: 'initial', height: '100%' }}>
-                    <div
-                        style={{
-                            padding: 24,
-                            textAlign: 'center',
-                            background: 'blue',
-                            borderRadius: '5px',
-                        }}
-                    >
-                        {children}
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
-                </Footer>
-            </Layout>
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
-        </Layout>
-    )
+  return (
+    <Layout className='page'>
+      <Header className='header'>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          className='header menu'
+        />
+      </Header>
+      <Content className='content'>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Finanças</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          {children}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+  );
 }
